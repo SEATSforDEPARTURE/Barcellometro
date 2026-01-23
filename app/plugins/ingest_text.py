6e69ee82a84b9925a896b46fbf6723eb4adc7147
SEATSforDEPARTURE <129211412+SEATSforDEPARTURE@users.noreply.github.com>
2026-01-23 11:51:19 +0100

    Update ingest_text.py

--- app/plugins/ingest_text.py
+++ app/plugins/ingest_text.py
@@ -15,7 +15,7 @@ from sqlalchemy import select
 from app.db.models.core import TextIngestChannelConfig, TextIngestMessage
 
 ROME_TZ = ZoneInfo("Europe/Rome")
-gging.getLogger("barcellometro.plugin.ingest_text")
+log = logging.getLogger("barcellometro.plugin.ingest_text")
 
 
 def get_manifest():
